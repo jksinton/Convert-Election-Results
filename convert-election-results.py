19e20e4a13c32e8dbf5b0070dac6d708c13ef0ad
James Sinton <jksinton@gmail.com>
2017-10-01 16:43:20 -0500

    changes from master

--- convert-election-results.py
+++ convert-election-results.py
@@ -18,10 +18,10 @@ import cv2
 import numpy as np
 import csv
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 def read_settings(args):
-    """Read the settings stored in settings.ini
+    """Processing the settings from commandline
     Args: 
         args: argparse.ArgumentParser object that stores command line arguments
     Returns: 
@@ -32,18 +32,26 @@ def read_settings(args):
     # Default values
     pdf_file = None
     image_file = None
+    first_page = None
+    last_page = None
     debug_is_on = False
     
     if args.pdf:
         pdf_file=args.pdf
     if args.image_file:
         image_file=args.image_file
+    if args.first_page:
+        first_page=int(args.first_page)
+    if args.last_page:
+        last_page=int(args.last_page)
     if args.debug:
         debug_is_on=args.debug
 
     settings = {
                 "pdf_file": pdf_file,
                 "image_file": image_file,
+                "first_page": first_page,
+                "last_page": last_page,
                 "debug_is_on": debug_is_on
             }
 
@@ -63,6 +71,8 @@ def get_command_line_args():
     parser = argparse.ArgumentParser(description='Convert election results to a computer readable format, e.g., csv, json, xml')
     parser.add_argument('-p','--pdf', help='PDF file to process')
     parser.add_argument('-i','--image-file', help='image file to process')
+    parser.add_argument('--first-page', help='page to begin processing')
+    parser.add_argument('--last-page', help='page to end processing')
     parser.add_argument('-v','--version',action='version', 
             version='%(prog)s %(version)s' % {"prog": parser.prog, "version": _version})
     parser.add_argument('-d','--debug',help='print debug messages',action="store_true")
@@ -70,17 +80,30 @@ def get_command_line_args():
     return parser.parse_args()
 
 
-def convert_election_results(pdf_file=None, image_file=None, debug_is_on=True):
+def convert_election_results(pdf_file=None, image_file=None, first_page=None, last_page=None, debug_is_on=True):
     """Convert the election results pdf to a computer readable format
     """
     if pdf_file is not None:
         pdf = PdfFileReader(open(pdf_file, 'rb'))
-        total_pages = pdf.getNumPages()
+        final_page = pdf.getNumPages()
+        total_pages = final_page
         old_office = ''
         office_data = []
-        for page_num in range(total_pages):
+        pages = []
+        if (last_page is not None) and (first_page is not None):
+            total_pages = (last_page+1) - first_page
+            pages = range(first_page, last_page+1)
+        elif first_page is not None:
+            total_pages = (final_page + 1) - first_page
+            last_page = final_page
+            pages = range(first_page, last_page+1)
+        else:
+            last_page = final_page
+            first_page = 1
+            pages = range(first_page, last_page+1)
+        for page_num in pages:
             tmp_tiff="tmp.tiff"
-            options="-sDEVICE=tiffgray -r300 -dINTERPOLATE -dFirstPage={page_num} -dLastPage={page_num} -dNumRenderingThreads=4 -sCompression=lzw -c 30000000 setvmthreshold".format(page_num=str(page_num + 1))
+            options="-sDEVICE=tiffgray -r300 -dINTERPOLATE -dFirstPage={page_num} -dLastPage={page_num} -dNumRenderingThreads=4 -sCompression=lzw -c 30000000 setvmthreshold".format(page_num=str(page_num))
             
             os.system('gs -o %(tiff)s  %(options)s -f "%(pdf)s" > /dev/null 2>&1' % {"pdf": pdf_file, "tiff": tmp_tiff, "options": options})
             
@@ -102,8 +125,15 @@ def convert_election_results(pdf_file=None, image_file=None, debug_is_on=True):
                     if 'Totals' not in line.split(' ')[0]:
                         line = line.replace('o','0')
                     office_data.append([office_text] + line.split(' '))
+                print type(page_num)
+                print type(last_page)
+                if page_num == last_page:
+                        print "writing last file"
+                        with open('csv/' + office_text.encode('utf-8') + '.csv', 'w') as f:
+                            writer = csv.writer(f)
+                            writer.writerows(office_data)
 
-                status = r"%s    %10d  [%3.2f%%]" % (office_text, page_num+1, (page_num+1) * 100. / total_pages)
+                status = r"%s    %10d  [%3.2f%%]" % (office_text, page_num, ((page_num + 1) - first_page) * 100. / total_pages)
                 status = status + chr(8)*(len(status)+1)
                 print status,
 
@@ -234,9 +264,17 @@ def main():
     settings = read_settings(args)
     pdf_file = settings['pdf_file']
     image_file = settings['image_file']
+    first_page = settings['first_page']
+    last_page = settings['last_page']
     debug_is_on = settings['debug_is_on']
 
-    convert_election_results(pdf_file=pdf_file, image_file=image_file, debug_is_on=debug_is_on)
+    convert_election_results(
+            pdf_file=pdf_file, 
+            image_file=image_file, 
+            first_page=first_page,
+            last_page=last_page,
+            debug_is_on=debug_is_on
+        )
 
 
 if __name__ == "__main__":
